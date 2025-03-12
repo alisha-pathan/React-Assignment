7a35b32b2f3492985c679d7631e23b15a6f198e9
alishapatha <alishapathan401@gmail.com>
2025-03-12 12:16:51 +0530

    Update Hookstask.js

--- src/Hookstask.js
+++ src/Hookstask.js
@@ -1,13 +1,4 @@
-//  <!-- Task 1: 
-// • Create a functional component with a counter using the useState() hook. Include 
-// buttons to increment and decrement the counter. 
-// • Task 2: 
-// • Use the useEffect() hook to fetch and display data from an API when the 
-// component mounts. 
-// • Task 3: 
-// • Create react app with use of useSelector & useDispatch.  
-// • Task 4: 
-// • Create react app to avoid re-renders in react application by useRef  ? -->
+ 
 
 import React, { useEffect, useState } from 'react'
 
